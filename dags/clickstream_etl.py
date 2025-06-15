601835248b25de5248b59ea25fd36c5074085301
KonstantineBiganashvili <konstantinebiganashvili@gmail.com>
2025-06-15 21:12:11 +0400

    Fix: datetime error in query

--- dags/clickstream_etl.py
+++ dags/clickstream_etl.py
@@ -6,12 +6,13 @@ from airflow.utils.dates import days_ago
 from datetime import timedelta
 
 RAW_BUCKET = os.environ.get("RAW_BUCKET")
+
 if not RAW_BUCKET:
     raise ValueError("RAW_BUCKET environment variable is not set")
 
 with DAG(
     dag_id="clickstream_etl",
-    schedule_interval="0 * * * *",
+    schedule_interval="@hourly",
     start_date=days_ago(1),
     catchup=False,
     default_args={"retries": 3, "retry_delay": timedelta(seconds=30)},
@@ -24,6 +25,7 @@ with DAG(
         destination_project_dataset_table="raw.clickstream",
         source_format="NEWLINE_DELIMITED_JSON",
         write_disposition="WRITE_APPEND",
+        time_partitioning={"type": "HOUR"},
     )
 
     transform_sql = BigQueryInsertJobOperator(
@@ -31,14 +33,15 @@ with DAG(
         configuration={
             "query": {
                 "query": """
-                   CREATE OR REPLACE TABLE analytics.sessions AS
-                   SELECT user_id,
-                          COUNTIF(event='add_to_cart') AS add_to_cart_events,
-                          COUNTIF(event='purchase')    AS purchases,
-                          APPROX_QUANTILES(latency_ms, 100)[SAFE_OFFSET(50)] AS p50_latency
-                   FROM  `raw.clickstream`
-                   WHERE _PARTITIONTIME = TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR)
-                   GROUP BY user_id;
+                    CREATE OR REPLACE TABLE analytics.sessions AS
+                    SELECT
+                        user_id,
+                        COUNTIF(event = 'add_to_cart') AS add_to_cart_events,
+                        COUNTIF(event = 'purchase')    AS purchases,
+                        APPROX_QUANTILES(latency_ms, 100)[SAFE_OFFSET(50)] AS p50_latency
+                    FROM `raw.clickstream`
+                    WHERE _PARTITIONTIME = TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR)
+                    GROUP BY user_id;
                 """,
                 "useLegacySql": False,
             }
